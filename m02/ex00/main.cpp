3559fe64cb4d8a58817922cb14fa3e519e7ea80c
Tiago <tiagofmpereira22@gmail.com>
2023-07-19 15:44:28 +0100

    m02 headers

--- m02/ex00/main.cpp
+++ m02/ex00/main.cpp
@@ -1,16 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 12:03:58 by timartin          #+#    #+#             */
+/*   Updated: 2023/07/19 12:03:59 by timartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int main(void)
 {
-    Fixed   a;
-    Fixed   b(a);
-    Fixed   c;
+	{
+		Fixed   a;
+		Fixed   b(a);
+		Fixed   c;
 
-    c = b;
+		c = b;
 
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		Fixed   a;
+		Fixed   b;
 
-    return 0;
+		a.setRawBits(10);
+		std::cout << a.getRawBits() << std::endl;
+		a.setRawBits(42);
+		std::cout << a.getRawBits() << std::endl;
+		b = a;
+		a.setRawBits(24);
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+	}
+
+	return 0;
 }
--- m02/ex01/main.cpp
+++ m02/ex01/main.cpp
@@ -1,21 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 12:04:51 by timartin          #+#    #+#             */
+/*   Updated: 2023/07/19 12:04:53 by timartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int main(void)
 {
-    Fixed       a;
-    Fixed const b(10);
-    Fixed const c(42.42f);
-    Fixed const d(b);
+	Fixed       a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-    a = Fixed(1234.4321f);
+	a = Fixed(1234.4321f);
 
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-    
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << std::endl;
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+	std::cout << std::endl;
 }
