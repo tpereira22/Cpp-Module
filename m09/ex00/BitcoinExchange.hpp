97b405793f71a2806a133277f624fa31d91464f3
Tiago Pereira <Tiago.Filipe.Pereira@criticaltechworks.com>
2024-01-29 18:42:03 +0000

    Keep goooing

--- m09/ex00/BitcoinExchange.hpp
+++ m09/ex00/BitcoinExchange.hpp
@@ -3,6 +3,8 @@
 
 # include <iostream>
 # include <fstream>
+# include <cctype>
+# include <cstdlib>
 # include <map>
 
 class BitcoinExchange
@@ -13,8 +15,13 @@ private:
     BitcoinExchange(BitcoinExchange const &copy);
     BitcoinExchange &operator=(BitcoinExchange const &copy);
 
-    static void checkInputFile(std::string filename);
-    static std::map<std::string, std::string> getDatabase(void);
+    static int checkInputFile(std::ifstream &file, std::string const filename);
+    static int checkIfDigit(std::string date);
+    static int checkIfDateExist(std::string month, std::string day);
+    static int checkDateFormat(std::string line);
+    static int checkIfValidLine(std::string line);
+    static void convertBitcoinValues(std::map<std::string, std::string> &database, std::string const filename);
+    static void getDatabase(std::map<std::string, std::string> &database);
 
 public:
     static void getBitcoinValues(std::string const filename);
